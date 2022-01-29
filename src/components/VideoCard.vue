32334fc69aca0589f86334ac396749408d6dcdd7
Murilo Ângelo <muriloangelo10@gmail.com>
2022-01-29 16:06:53 -0300

    creating a tested video card component

---
+++ src/components/VideoCard.vue
@@ -0,0 +1,59 @@
+<template>
+  <b-card :title="title" :img-src="thumb" img-alt="Image" img-top align="left">
+    <b-card-text>
+      {{ channelName }}<br />
+      <small class="text-muted">
+        Tags:
+        {{ formatedTags }}
+      </small>
+    </b-card-text>
+
+    <template #footer>
+      <div class="d-flex justify-content-between">
+        <div>Views: {{ viewCounter }}</div>
+        <div>Likes: {{ likeCounter }}</div>
+        <div>Comments: {{ commentCounter }}</div>
+      </div>
+    </template>
+  </b-card>
+</template>
+
+<script>
+export default {
+  name: "VideoCard",
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+    channelName: {
+      type: String,
+      default: "",
+    },
+    tags: {
+      type: Array,
+    },
+    viewCounter: {
+      type: Number,
+      default: 0,
+    },
+    likeCounter: {
+      type: Number,
+      default: 0,
+    },
+    commentCounter: {
+      type: Number,
+      default: 0,
+    },
+    thumb: {
+      type: String,
+      default: "",
+    },
+  },
+  computed: {
+    formatedTags() {
+      return this.tags.slice(0, 5).join(", ");
+    },
+  },
+};
+</script>
