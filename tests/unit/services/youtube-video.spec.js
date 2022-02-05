541f6d2f94a3a8ac2fe7bc650b2da5f7203b24c4
Murilo Ângelo <muriloangelo10@gmail.com>
2022-02-05 16:24:21 -0300

    testing featuredVideos method on YoutubeVideo service

--- tests/unit/services/youtube-video.spec.js
+++ tests/unit/services/youtube-video.spec.js
@@ -29,4 +29,33 @@ describe("YoutubeVideo service", () => {
 
     expect(window.gapi.client.youtube.search.list().execute).toHaveBeenCalled();
   });
+
+  test("featuredVideos should call gapi.client.youtube.videos.list method and resolve promise", async () => {
+    window.gapi = {
+      client: {
+        youtube: {
+          videos: {
+            list: jest.fn().mockReturnValue({
+              execute: jest.fn(),
+            }),
+          },
+        },
+      },
+    };
+
+    const payload = {
+      part: "snippet, contentDetails, statistics",
+      chart: "mostPopular",
+      regionCode: "BR",
+      maxResults: 4,
+    };
+
+    YoutubeVideo.featuredVideos();
+
+    expect(window.gapi.client.youtube.videos.list).toHaveBeenCalledWith(
+      payload
+    );
+
+    expect(window.gapi.client.youtube.videos.list().execute).toHaveBeenCalled();
+  });
 });
